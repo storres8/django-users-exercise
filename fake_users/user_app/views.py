f76082096613462a65bbccf4855734b9c213fd26
storres8 <stevetorres7562@gmail.com>
2018-12-18 15:31:04 -0500

    added to the form.

--- fake_users/user_app/views.py
+++ fake_users/user_app/views.py
@@ -13,5 +13,18 @@ def users(request):
 
 # for forms
 def form_name(request):
-    form = forms.FormName()    
+    form = forms.FormName()
+
+    if request.method == "POST":
+        # allows you to access the form data
+        form = forms.FormName(request.POST)
+        # if the form is valid run this code which prints what your wrote into terminal
+        if form.is_valid():
+            print("Form submited!")
+            print(f"Name: {form.cleaned_data['name']}")
+            print(f"Email: {form.cleaned_data['email']}")
+            print(f"Text: {form.cleaned_data['text']}")
+        else:
+            # do nothing, just leave the form unbound
+            form.FormName()
     return render(request, 'user_app/form.html', {'form': form})
