e035444d5ccdbecc36198c3bf7b75d1d51104f33
storres8 <stevetorres7562@gmail.com>
2018-12-18 17:02:09 -0500

    added two custom built validations to the form.

--- fake_users/user_app/forms.py
+++ fake_users/user_app/forms.py
@@ -1,6 +1,7 @@
 from django import forms
 from django.core import validators
 
+
 class FormName(forms.Form):
     name = forms.CharField(max_length=128)
     email = forms.EmailField(widget=forms.EmailInput)
@@ -12,5 +13,10 @@ class FormName(forms.Form):
         email = cleaned_data['email']
         vemail = cleaned_data['verify_email']
 
+        # checks to make sure the emails match
         if email != vemail:
             raise forms.ValidationError("emails do not match, please verify")
+
+        # check to make sure there is at least one number in the emails
+        if any(char.isdigit() for char in email) != True:
+            raise forms.ValidationError("email must contain at least one number")
